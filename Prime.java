60e582fdd9ade272bdf39c6d6d32840314061082
TCTrung <trungtrinh1325@gmail.com>
2023-10-10 14:06:36 +0700

    hi

---
+++ Prime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so:");
+        int number = scanner.nextInt();
+        if (number < 2){
+            System.out.println(number + " khong phai so nguyen to");
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                System.out.println(number + " la so nguyen to");
+            else
+                System.out.println(number + " khong phai so nguyen to");
+        }
+    }
+}
